541fcec2cda4590fe0ddc5baaed22bde8e70632d
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-11-14 11:39:22 +0100

    factory function

---
+++ oop.js
@@ -0,0 +1,44 @@
+// const Color = function (r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// function hex(r, g, b) {
+//   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// function rgb(r, g, b) {
+//   return `rgb(${r}, ${g}, ${b})`
+// }
+
+function makeColor(r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`
+  };
+
+  color.hex = function () {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+  return color;
+}
+
+const firstColor = makeColor(220, 34, 96);
+firstColor.hex();
+firstColor.rgb();
+
+const secondColor = makeColor(20, 134, 196);
+secondColor.hex();
+secondColor.rgb();
+
+const thirdColor = makeColor(124, 8, 220);
+thirdColor.hex();
+thirdColor.rgb();
