a113e54418999922052a9eab587204af6dbeeb0c
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-11-19 10:39:02 +0100

    class constructor continued

---
+++ extendsandsuper.js
@@ -0,0 +1,27 @@
+
+
+class Cat {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating!`;
+  }
+  meow() {
+    returns`${this.name} Meows when it is tired.`;
+  }
+}
+
+class Dog {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating!`;
+  }
+  bark() {
+    return `${this.name} has been barking all night!`;
+  }
+}
